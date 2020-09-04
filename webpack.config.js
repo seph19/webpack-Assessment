1c0d988749925be6fc60af01ce1fdff9844921df
seph <joseph.baron@devpartners.co>
2020-09-04 23:27:39 +0800

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,33 @@
-
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = (env) => {
+  const { NODE_ENV } = env;
+  const isProd = NODE_ENV === 'production';
+  return {
+    entry: {
+      main: './src/js',
+      vendor1: './src/js/vendors/vendor1',
+      vendor2: './src/js/vendors/vendor2',
+    },
+    output: {
+      path: path.resolve(__dirname, './build'),
+      filename: isProd ? '[name].[hash].bundle.js' : '[name].bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './public/index.html',
+      }),
+    ],
+  };
+};
